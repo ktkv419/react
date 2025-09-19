bf7d5ecdb4833ea07e30a5ab03d5b42fa57e075f
Pasha <39154112+ktkv419@users.noreply.github.com>
2025-09-19 07:56:52 +0000

    finished

--- movie-db/src/App.jsx
+++ movie-db/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Movie from './components/Movie/Movie'
 import { genres } from './utils/genres'
@@ -6,10 +6,25 @@ import { genres } from './utils/genres'
 function App() {
   const [movie, setMovie] = useState("")
   const [score, setScore] = useState(2)
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState(() => {
+    return JSON.parse(
+      localStorage.getItem("movies")
+    ) || []
+  })
   const [desc, setDesc] = useState("")
   const [genre, setGenre] = useState(Object.entries(genres)[0][0])
+  const [filteredMovies, setFilteredMovies] = useState(movies)
+  const [filterString, setFilterString] = useState("")
 
+  useEffect(() => {
+    setFilteredMovies(movies.filter(
+      (movie) => movie.name.includes(filterString))
+    )
+  }, [filterString, movies])
+
+  useEffect(() => {
+    localStorage.setItem("movies", JSON.stringify(movies))
+  }, [movies])
 
   const handleAdd = (e) => {
     e.preventDefault()
@@ -30,10 +45,10 @@ function App() {
     <div className="app">
       <h1>Movie DB</h1>
       <form onSubmit={handleAdd} action="" className="movie-form">
-        <input type="text" onChange={(value) => setMovie(value.target.value)} value={movie}/>
-        <input type="range" min={0} max={4}  value={score} onChange={(v) => setScore(v.target.value)}/>
+        <input type="text" onChange={(value) => setMovie(value.target.value)} value={movie} />
+        <input type="range" min={0} max={4} value={score} onChange={(v) => setScore(v.target.value)} />
         {+score + 1}
-        <select>
+        <select value={genre} onChange={(v) => setGenre(v.target.value)}>
           {Object.entries(genres).map((genre) => (
             <option value={genre[0]}>{genre[1]}</option>
           ))}
@@ -41,8 +56,14 @@ function App() {
         <textarea value={desc} onChange={(value) => setDesc(value.target.value)}></textarea>
         <button className="add-movie">Добавить</button>
       </form>
+      <div className="search">
+        <h2>Поиск</h2>
+        <input type="text" value={filterString} onChange={
+          (v) => setFilterString(v.target.value)
+        } />
+      </div>
       <div className="movie-list">
-        {movies.map((el) => (
+        {filteredMovies.map((el) => (
           <Movie key={el.id} {...el} />
         ))}
       </div>
--- movie-db/src/components/Movie/Movie.jsx
+++ movie-db/src/components/Movie/Movie.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react'
 import './Movie.css'
+import { genres } from '../../utils/genres'
 
 const Movie = ({name, genre, score, desc}) => {
     return (
         <div className="movie">
             <h4>{name}</h4>
             <p>{desc}</p>
-            <p>{genre}</p>
+            <p>{genres[genre]}</p>
             <div className="movie__score">
                 {Array(5).fill().map((_, i) => score < i ? "☆" : "★")}
             </div>
